2bbffe5d730cda98396041feb0db60bda53f8aa6
Manish Jadaun <manish.jadaun2606@gmail.com>
2026-09-10 15:57:23 +0530

    Update database connection for deployment

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
 import os
 import MySQLdb
 
+
 def get_connection():
     connection = MySQLdb.connect(
-        host=os.getenv("DB_HOST", "localhost"),
-        user=os.getenv("DB_USER", "root"),
-        passwd=os.getenv("DB_PASSWORD"),
-        db=os.getenv("DB_NAME", "hotel_management"),
-        port=int(os.getenv("DB_PORT", "3306"))
+        host=os.environ["DB_HOST"],
+        user=os.environ["DB_USER"],
+        passwd=os.environ["DB_PASSWORD"],
+        db=os.environ["DB_NAME"],
+        port=int(os.environ.get("DB_PORT", "3306"))
     )
+
     return connection
